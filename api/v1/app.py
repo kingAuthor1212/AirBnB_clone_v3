36b3a5d2cfdf0460a7446f7395ed5f9c64ac73f5
kingAuthor1212 <kingauthor1212@gmail.com>
2023-05-01 11:40:55 -0700

    clone_3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,39 @@
 #!/usr/bin/python3
-"""Flask server (variable app)
-"""
+"""Definition of the API server"""
 
 
-from flask import Flask, jsonify
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from flasgger import Swagger, swag_from
 from models import storage
 from os import getenv
-from api.v1.views import app_views
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+CORS(app, origins='0.0.0.0', resources='/*')
+swagger = Swagger(app)
+swagger.template = swagger.load_swagger_file('../../docs/api.yml')
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def closeStorageAfterRequest(error):
+    """Close and reload the storage device between requests"""
     storage.close()
+    storage.reload()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
+def error404(error):
+    """Use a JSON rather than HTML response when a URL is not found"""
+    return make_response(jsonify(error="Not found"), 404)
 
 
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    app.run(
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=getenv('HBNB_API_PORT', 5000),
+        threaded=True
+    )
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,25 @@
-#!/usr/bin/python3xx
-'''api status'''
-import models
-from models import storage
-from models.base_model import BaseModel
-from flask import jsonify
+#!/usr/bin/python3
+"""The entry point for the API"""
+
+
 from api.v1.views import app_views
+import flask
+import models
+import models.base_model
 
 
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return stuff'''
-    return jsonify(status='OK')
+@app_views.route('/status')
+def api_status():
+    """Report that the API is available"""
+    return flask.jsonify(status='OK')
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+@app_views.route('/stats')
+def api_countModels():
+    """Show the counts of each model type in storage"""
+    counts = {
+        cls.__tablename__: models.storage.count(name)
+        for name, cls in models.classes.items()
+        if issubclass(cls, models.base_model.Base)
+    }
+    return flask.jsonify(counts)
