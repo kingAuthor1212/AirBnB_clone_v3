40373f22b98aace549571ef300794b38119711c6
kingAuthor1212 <kingauthor1212@gmail.com>
2023-05-02 03:28:54 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-"""Definition of the API server"""
+"""Flask server (variable app)"""
 
 
-from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
-from flasgger import Swagger, swag_from
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
-
+from api.v1.views import app_views
 
 app = Flask(__name__)
-CORS(app, origins='0.0.0.0', resources='/*')
-swagger = Swagger(app)
-swagger.template = swagger.load_swagger_file('../../docs/api.yml')
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def closeStorageAfterRequest(error):
-    """Close and reload the storage device between requests"""
+def downtear(self):
+    '''Status of your API'''
     storage.close()
-    storage.reload()
 
 
 @app.errorhandler(404)
-def error404(error):
-    """Use a JSON rather than HTML response when a URL is not found"""
-    return make_response(jsonify(error="Not found"), 404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
 
 
-if __name__ == '__main__':
-    app.run(
-        host=getenv('HBNB_API_HOST', '0.0.0.0'),
-        port=getenv('HBNB_API_PORT', 5000),
-        threaded=True
-    )
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
